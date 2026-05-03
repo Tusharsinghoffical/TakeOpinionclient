131fbca15425da72c554a2eb8a8d706fc4f533f5
Tusharsinghoffical <tusharsinghoffical@users.noreply.github.com>
2026-05-03 21:53:08 +0530

    Fix: website fully public - all data visible without login

--- takeopinion/urls.py
+++ takeopinion/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for takeopinion project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -28,7 +11,6 @@ urlpatterns = [
     path("api/v1/entities/<str:entity_type>/", csrf_exempt(get_entities), name='get_entities'),
 ]
 
-# Add internationalized patterns
 urlpatterns += i18n_patterns(
     path("", include("core.urls")),
     path("treatments/", include("treatments.urls")),
